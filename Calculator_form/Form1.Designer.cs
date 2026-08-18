000d55719ad17ac99a7bfd03b97e86e8e24bc75d
student <student@V1-6>
2026-08-18 15:32:12 +0800

    Add project files.

---
+++ Calculator_form/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Calculator_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCE = new Button();
+            btnC = new Button();
+            btnEqual = new Button();
+            btndiv = new Button();
+            btnMult = new Button();
+            num9 = new Button();
+            num8 = new Button();
+            num7 = new Button();
+            num4 = new Button();
+            num5 = new Button();
+            num6 = new Button();
+            btnAdd = new Button();
+            btnMinus = new Button();
+            num3 = new Button();
+            num2 = new Button();
+            num1 = new Button();
+            lblResult = new Label();
+            SuspendLayout();
+            // 
+            // btnCE
+            // 
+            btnCE.BackColor = SystemColors.ScrollBar;
+            btnCE.Location = new Point(15, 140);
+            btnCE.Name = "btnCE";
+            btnCE.Size = new Size(91, 61);
+            btnCE.TabIndex = 0;
+            btnCE.Text = "CE";
+            btnCE.UseVisualStyleBackColor = false;
+            btnCE.Click += btnCE_Click;
+            // 
+            // btnC
+            // 
+            btnC.BackColor = SystemColors.ScrollBar;
+            btnC.Location = new Point(112, 140);
+            btnC.Name = "btnC";
+            btnC.Size = new Size(91, 61);
+            btnC.TabIndex = 0;
+            btnC.Text = "C";
+            btnC.UseVisualStyleBackColor = false;
+            btnC.Click += btnC_Click;
+            // 
+            // btnEqual
+            // 
+            btnEqual.BackColor = SystemColors.ScrollBar;
+            btnEqual.Location = new Point(209, 140);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new Size(91, 61);
+            btnEqual.TabIndex = 0;
+            btnEqual.Text = "=";
+            btnEqual.UseVisualStyleBackColor = false;
+            btnEqual.Click += button3_Click;
+            // 
+            // btndiv
+            // 
+            btndiv.BackColor = SystemColors.ScrollBar;
+            btndiv.Location = new Point(306, 140);
+            btndiv.Name = "btndiv";
+            btndiv.Size = new Size(91, 61);
+            btndiv.TabIndex = 0;
+            btndiv.Text = "/";
+            btndiv.UseVisualStyleBackColor = false;
+            // 
+            // btnMult
+            // 
+            btnMult.BackColor = SystemColors.ScrollBar;
+            btnMult.Location = new Point(306, 220);
+            btnMult.Name = "btnMult";
+            btnMult.Size = new Size(91, 61);
+            btnMult.TabIndex = 0;
+            btnMult.Text = "X";
+            btnMult.UseVisualStyleBackColor = false;
+            // 
+            // num9
+            // 
+            num9.BackColor = SystemColors.ScrollBar;
+            num9.Location = new Point(209, 220);
+            num9.Name = "num9";
+            num9.Size = new Size(91, 61);
+            num9.TabIndex = 0;
+            num9.Text = "9";
+            num9.UseVisualStyleBackColor = false;
+            // 
+            // num8
+            // 
+            num8.BackColor = SystemColors.ScrollBar;
+            num8.Location = new Point(112, 220);
+            num8.Name = "num8";
+            num8.Size = new Size(91, 61);
+            num8.TabIndex = 0;
+            num8.Text = "8";
+            num8.UseVisualStyleBackColor = false;
+            // 
+            // num7
+            // 
+            num7.BackColor = SystemColors.ScrollBar;
+            num7.Location = new Point(15, 220);
+            num7.Name = "num7";
+            num7.Size = new Size(91, 61);
+            num7.TabIndex = 0;
+            num7.Text = "7";
+            num7.UseVisualStyleBackColor = false;
+            num7.Click += num7_Click;
+            // 
+            // num4
+            // 
+            num4.BackColor = SystemColors.ScrollBar;
+            num4.Location = new Point(15, 303);
+            num4.Name = "num4";
+            num4.Size = new Size(91, 61);
+            num4.TabIndex = 0;
+            num4.Text = "4";
+            num4.UseVisualStyleBackColor = false;
+            // 
+            // num5
+            // 
+            num5.BackColor = SystemColors.ScrollBar;
+            num5.Location = new Point(112, 303);
+            num5.Name = "num5";
+            num5.Size = new Size(91, 61);
+            num5.TabIndex = 0;
+            num5.Text = "5";
+            num5.UseVisualStyleBackColor = false;
+            // 
+            // num6
+            // 
+            num6.BackColor = SystemColors.ScrollBar;
+            num6.Location = new Point(209, 303);
+            num6.Name = "num6";
+            num6.Size = new Size(91, 61);
+            num6.TabIndex = 0;
+            num6.Text = "6";
+            num6.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = SystemColors.ScrollBar;
+            btnAdd.Location = new Point(306, 303);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(91, 61);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "+";
+            btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnMinus
+            // 
+            btnMinus.BackColor = SystemColors.ScrollBar;
+            btnMinus.Location = new Point(306, 383);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(91, 61);
+            btnMinus.TabIndex = 0;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = false;
+            // 
+            // num3
+            // 
+            num3.BackColor = SystemColors.ScrollBar;
+            num3.Location = new Point(209, 383);
+            num3.Name = "num3";
+            num3.Size = new Size(91, 61);
+            num3.TabIndex = 0;
+            num3.Text = "3";
+            num3.UseVisualStyleBackColor = false;
+            // 
+            // num2
+            // 
+            num2.BackColor = SystemColors.ScrollBar;
+            num2.Location = new Point(112, 383);
+            num2.Name = "num2";
+            num2.Size = new Size(91, 61);
+            num2.TabIndex = 0;
+            num2.Text = "2";
+            num2.UseVisualStyleBackColor = false;
+            // 
+            // num1
+            // 
+            num1.BackColor = SystemColors.ScrollBar;
+            num1.Location = new Point(15, 383);
+            num1.Name = "num1";
+            num1.Size = new Size(91, 61);
+            num1.TabIndex = 0;
+            num1.Text = "1";
+            num1.UseVisualStyleBackColor = false;
+            num1.Click += num1_Click;
+            // 
+            // lblResult
+            // 
+            lblResult.BorderStyle = BorderStyle.FixedSingle;
+            lblResult.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblResult.Location = new Point(15, 44);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(382, 53);
+            lblResult.TabIndex = 1;
+            lblResult.Text = "\r\n0.0";
+            lblResult.TextAlign = ContentAlignment.MiddleRight;
+            lblResult.Click += lblResult_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(416, 456);
+            Controls.Add(lblResult);
+            Controls.Add(num1);
+            Controls.Add(num2);
+            Controls.Add(num3);
+            Controls.Add(btnMinus);
+            Controls.Add(btnAdd);
+            Controls.Add(num6);
+            Controls.Add(num5);
+            Controls.Add(num4);
+            Controls.Add(num7);
+            Controls.Add(num8);
+            Controls.Add(num9);
+            Controls.Add(btnMult);
+            Controls.Add(btndiv);
+            Controls.Add(btnEqual);
+            Controls.Add(btnC);
+            Controls.Add(btnCE);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnCE;
+        private Button btnC;
+        private Button btnEqual;
+        private Button btndiv;
+        private Button btnMult;
+        private Button num9;
+        private Button num8;
+        private Button num7;
+        private Button num4;
+        private Button num5;
+        private Button num6;
+        private Button btnAdd;
+        private Button btnMinus;
+        private Button num3;
+        private Button num2;
+        private Button num1;
+        private Label lblResult;
+    }
+}
---
+++ Calculator_form/Form1.cs
@@ -0,0 +1,43 @@
+namespace Calculator_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double resultValue = 0;
+        string operationPerformed = "";
+        bool OperationPer = false;
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCE_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "0";
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "0";
+
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
